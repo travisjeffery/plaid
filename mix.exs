6ececd822b4e10f26bdd868dade2ea61d87fba17
Travis Jeffery <tj@travisjeffery.com>
2016-04-03 01:49:58 -0500

    flush out pkg

--- mix.exs
+++ mix.exs
@@ -5,31 +5,28 @@ defmodule Plaid.Mixfile do
     [app: :plaid,
      version: "0.0.1",
      elixir: "~> 1.2",
+     description: "Client for Plaid, the finance API.",
+     package: package,     
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:httpoison, "~> 0.8.0"},
       {:poison, "~> 2.0"}
     ]
+  end
+
+  defp package do
+    files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+    maintainers: ["Travis Jeffery"],
+    licenses: ["MIT"],
+    links: %{"GitHub" => "https://github.com/travisjeffery/plaid"}
   end
 end
